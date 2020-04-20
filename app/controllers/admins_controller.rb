74d7e6b36f1a4068782950045892925017205d12
FUENTES <fuentes@lui.home>
2020-04-20 14:42:00 -0400

    schema

--- app/controllers/admins_controller.rb
+++ app/controllers/admins_controller.rb
@@ -1,5 +1,5 @@
 class AdminsController < ApplicationController
-  before_action :set_admin, only: [:create, :show, :update, :destroy]
+  before_action :set_admin, only: [:show, :update, :destroy]
   before_action :authorize_request, except: [:index, :show]
 
   # GET /admins
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,9 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+
+
+Admin.create!(username: 'Dustin', email: "dusty@gmail.com", password: "123457" )
+
+User.create!(username: 'Brandon', email: "brandonstudenta@gmail.com", password: "123457" )
